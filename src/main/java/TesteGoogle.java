ca9024f14921b2ff04f61936868658485521f6db
wellington_197 <fcowellcosta@gmail.com>
2023-02-07 08:34:17 -0300

    Subindo arquivos de correção do driver do Firefox

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,9 +1,12 @@
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
-	
-	public static void main(String[] args) {
+	@Test
+	public void teste(){
+		
+		//System.setProperty("webdriver.gecko.driver","/Users/welli/xampp/htdocs/CURSOS_WELLINGTON/CURSOS_UDEMY/Selenium/Drivers/geckodriver.exe");
 		WebDriver driver=new FirefoxDriver();
 		
 		driver.get("http://www.google.com");
